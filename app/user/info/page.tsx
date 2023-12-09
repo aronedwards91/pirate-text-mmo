f1c8363bc3226ef10f54c27ce355b8880b4b4815
aron edwards <aronedwards91@gmail.com>
2023-12-09 12:54:41 +0000

    protected routes

---
+++ app/user/info/page.tsx
@@ -0,0 +1,3 @@
+export default function UserInfoPage() {
+  return <div>Hi -- Info</div>;
+}
--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,19 @@
-import { NextResponse, type NextRequest } from 'next/server'
-import { createClient } from '@/utils/supabase/middleware'
+import { NextResponse, type NextRequest } from "next/server";
+import { createClient } from "@/utils/supabase/middleware";
 
+export const config = {
+  matcher: ["/user/:path*", "/play"],
+};
 export async function middleware(request: NextRequest) {
-  try {
-    // This `try/catch` block is only here for the interactive tutorial.
-    // Feel free to remove once you have Supabase connected.
-    const { supabase, response } = createClient(request)
+  const { supabase, response } = createClient(request);
 
-    // Refresh session if expired - required for Server Components
-    // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-session-with-middleware
-    await supabase.auth.getSession()
+  // Refresh session if expired - required for Server Components
+  // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-session-with-middleware
+  const auth = await supabase.auth.getSession();
 
-    return response
-  } catch (e) {
-    // If you are here, a Supabase client could not be created!
-    // This is likely because you have not set up environment variables.
-    // Check out http://localhost:3000 for Next Steps.
-    return NextResponse.next({
-      request: {
-        headers: request.headers,
-      },
-    })
+  if (auth.data.session) {
+    return NextResponse.next();
+  } else {
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 }
