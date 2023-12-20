7f8f019b50f1193bfd416e990c2ee795a0ef3986
aron edwards <aronedwards91@gmail.com>
2023-12-20 10:13:03 +0000

    Middleware update

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,8 +17,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "MARS",
+  description: "MARS - the online martian RPG",
 };
 
 export default function RootLayout({
--- middleware.ts
+++ middleware.ts
@@ -2,17 +2,36 @@ import { NextResponse, type NextRequest } from "next/server";
 import { createClient } from "@/utils/supabase/middleware";
 
 export const config = {
-  matcher: ["/user/:path*", "/play"],
+  matcher: ["/user/:path*", "/play", "/setup"],
 };
 export async function middleware(request: NextRequest) {
+
   const { supabase, response } = createClient(request);
 
   // Refresh session if expired - required for Server Components
   // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-session-with-middleware
-  const auth = await supabase.auth.getSession();
+  // const auth = await supabase.auth.getSession();
+  const [auth, user] = await Promise.all([supabase.auth.getSession(), supabase.auth.getUser()]);
 
   if (auth.data.session) {
-    return NextResponse.next();
+    // Save to cookies
+    const { data, error } = await supabase
+    .from("account-data")
+    .select()
+    .eq('user_id', user.data.user?.id);
+    // console.log('MW-data', data)
+
+    const isTargetSetup = request.url.includes('/setup');
+
+    if(data?.length === 0 && !isTargetSetup) {
+      return NextResponse.redirect(new URL("/setup", request.url));
+    } else {
+      if(isTargetSetup) {
+        return NextResponse.redirect(new URL("/user/info", request.url));
+      } else {
+        return NextResponse.next();
+      }
+    }
   } else {
     return NextResponse.redirect(new URL("/login", request.url));
   }
